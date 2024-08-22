8154743850d58fe61f458069f6a0b650a85469e3
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-08-22 23:05:43 +0700

    EF-01: Update Flutter version, add search bar widget

--- lib/pages/resource/image_paths.dart
+++ lib/pages/resource/image_paths.dart
@@ -3,8 +3,8 @@ import 'package:ecoparking_flutter/pages/resource/assets_paths.dart';
 class ImagePaths {
   static String get imgToyotaLandCruiser =>
       _getImagePath('img_toyota_land_cruiser.png');
-
   static String get imgGoogle => _getImagePath('img_google.svg');
+  static String get icFilter => _getIconPath('ic_filter.svg');
 
   static String _getImagePath(String imageName) {
     return AssetsPaths.images + imageName;
