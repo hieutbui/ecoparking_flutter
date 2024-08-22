9655c1bf499eeca7ff8d0aa98e472a0a7d5aba2e
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-08-23 00:11:36 +0700

    EF-01: Add bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:ecoparking_flutter/config/themes.dart';
 import 'package:ecoparking_flutter/pages/resource/image_paths.dart';
+import 'package:ecoparking_flutter/utils/bottom_sheet_utils.dart';
 import 'package:ecoparking_flutter/widgets/action_button/action_button.dart';
 import 'package:ecoparking_flutter/widgets/info_rectangle/info_rectangle.dart';
 import 'package:ecoparking_flutter/widgets/search_bar/search_bar.dart';
 import 'package:ecoparking_flutter/widgets/selection_card/selection_card.dart';
 import 'package:ecoparking_flutter/widgets/theme_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,117 +24,153 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           title: 'Flutter Demo',
           themeMode: themeMode,
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en', 'US'),
+          ],
           theme: EcoParkingThemes.buildTheme(context),
-          home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              title: const Text('Flutter Demo'),
-            ),
-            body: SingleChildScrollView(
-              physics: const ClampingScrollPhysics(),
-              child: Column(
-                children: <Widget>[
-                  const Text('Action Button'),
-                  ActionButton(
-                    type: ActionButtonType.positive,
-                    label: 'Details',
-                    isShowArrow: true,
-                    width: 175,
-                    arrowSize: 16,
-                    onPressed: () {},
-                  ),
-                  ActionButton(
-                    type: ActionButtonType.negative,
-                    label: 'Cancel',
-                    isShowArrow: true,
-                    width: 175,
-                    onPressed: () {},
-                  ),
-                  ActionButton(
-                    type: ActionButtonType.hollow,
-                    label: 'Submit',
-                    isShowArrow: false,
-                    width: 175,
-                    onPressed: () {},
-                  ),
-                  const SizedBox(height: 16),
-                  const Text('Info Rectangle'),
-                  const InfoRectangle(
-                    type: InfoRectangleType.filled,
-                    label: 'Filled Rectangle',
-                    padding: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 16,
-                    ),
-                    icon: Icons.location_on,
-                  ),
-                  const InfoRectangle(
-                    type: InfoRectangleType.hollow,
-                    label: 'Hollow Rectangle',
-                    padding: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 16,
-                    ),
-                    icon: Icons.punch_clock_rounded,
-                  ),
-                  const SizedBox(height: 16),
-                  const Text('Selection Card'),
-                  SelectionCard(
-                    title: 'Toyota  Land Cruiser',
-                    subtitle: 'AFD 6397',
-                    trailingImage: ImagePaths.imgToyotaLandCruiser,
-                    isSelected: true,
-                    isShowSelectCircle: true,
-                    titleColor: Theme.of(context).colorScheme.onSurface,
-                    subtitleColor: Theme.of(context)
-                        .colorScheme
-                        .onSurface
-                        .withOpacity(0.5),
-                  ),
-                  const SizedBox(height: 22),
-                  SelectionCard(
-                    title: 'Google Pay',
-                    trailingSVG: ImagePaths.imgGoogle,
-                    isSelected: false,
-                    isShowSelectCircle: true,
-                    titleColor: Theme.of(context).colorScheme.onSurface,
-                  ),
-                  const SizedBox(height: 16),
-                  const Text('Search Bar'),
-                  SearchAnchor(
-                    builder: (context, controller) {
-                      return AppSearchBar(
-                        controller: controller,
-                        onChanged: (String value) {
-                          print(value);
-                        },
-                        onTap: () {
-                          controller.openView();
-                        },
-                        isShowFilter: true,
-                        onFilterPressed: () {
-                          print('Filter pressed');
-                        },
-                      );
-                    },
-                    suggestionsBuilder: (context, controller) {
-                      return List<ListTile>.generate(5, (int index) {
-                        final String item = 'Item $index';
-                        return ListTile(
-                          title: Text(item),
-                          onTap: () {
-                            controller.closeView(item);
-                          },
-                        );
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
+          home: const TestPage(),
         );
       },
+    );
+  }
+}
+
+class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Flutter Demo'),
+      ),
+      body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Column(
+          children: <Widget>[
+            const Text('Action Button'),
+            ActionButton(
+              type: ActionButtonType.positive,
+              label: 'Details',
+              isShowArrow: true,
+              width: 175,
+              arrowSize: 16,
+              onPressed: () {},
+            ),
+            ActionButton(
+              type: ActionButtonType.negative,
+              label: 'Cancel',
+              isShowArrow: true,
+              width: 175,
+              onPressed: () {},
+            ),
+            ActionButton(
+              type: ActionButtonType.hollow,
+              label: 'Submit',
+              isShowArrow: false,
+              width: 175,
+              onPressed: () {},
+            ),
+            const SizedBox(height: 16),
+            const Text('Info Rectangle'),
+            const InfoRectangle(
+              type: InfoRectangleType.filled,
+              label: 'Filled Rectangle',
+              padding: EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 16,
+              ),
+              icon: Icons.location_on,
+            ),
+            const InfoRectangle(
+              type: InfoRectangleType.hollow,
+              label: 'Hollow Rectangle',
+              padding: EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 16,
+              ),
+              icon: Icons.punch_clock_rounded,
+            ),
+            const SizedBox(height: 16),
+            const Text('Selection Card'),
+            SelectionCard(
+              title: 'Toyota  Land Cruiser',
+              subtitle: 'AFD 6397',
+              trailingImage: ImagePaths.imgToyotaLandCruiser,
+              isSelected: true,
+              isShowSelectCircle: true,
+              titleColor: Theme.of(context).colorScheme.onSurface,
+              subtitleColor:
+                  Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+            ),
+            const SizedBox(height: 22),
+            SelectionCard(
+              title: 'Google Pay',
+              trailingSVG: ImagePaths.imgGoogle,
+              isSelected: false,
+              isShowSelectCircle: true,
+              titleColor: Theme.of(context).colorScheme.onSurface,
+            ),
+            const SizedBox(height: 16),
+            const Text('Search Bar'),
+            SearchAnchor(
+              builder: (context, controller) {
+                return AppSearchBar(
+                  controller: controller,
+                  onChanged: (String value) {
+                    print(value);
+                  },
+                  onTap: () {
+                    controller.openView();
+                  },
+                  isShowFilter: true,
+                  onFilterPressed: () {
+                    print('Filter pressed');
+                  },
+                );
+              },
+              suggestionsBuilder: (context, controller) {
+                return List<ListTile>.generate(5, (int index) {
+                  final String item = 'Item $index';
+                  return ListTile(
+                    title: Text(item),
+                    onTap: () {
+                      controller.closeView(item);
+                    },
+                  );
+                });
+              },
+            ),
+            const SizedBox(height: 16),
+            const Text('Bottom sheet'),
+            ActionButton(
+              type: ActionButtonType.positive,
+              label: 'Show Bottom Sheet',
+              width: 175,
+              onPressed: () => BottomSheetUtils.showBottomSheet(
+                context: context,
+                isDismissible: true,
+                isScrollControlled: true,
+                showDragHandle: true,
+                builder: (context) {
+                  return Container(
+                    color: Colors.amber,
+                    child: const Center(
+                      child: Text('Bottom Sheet'),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
