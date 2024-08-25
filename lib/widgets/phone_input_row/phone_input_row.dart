5368e67a7f4782327d9a97e285843bc81989882b
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-08-25 16:07:47 +0700

    EF-01: Add phone input row

---
+++ lib/widgets/phone_input_row/phone_input_row.dart
@@ -0,0 +1,57 @@
+import 'package:ecoparking_flutter/widgets/phone_input_row/phone_input_row_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:phone_form_field/phone_form_field.dart';
+
+class PhoneInputRow extends StatelessWidget {
+  final void Function(PhoneNumber?)? onChanged;
+
+  const PhoneInputRow({
+    super.key,
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PhoneFormField(
+      controller: PhoneController(
+        initialValue: const PhoneNumber(isoCode: IsoCode.VN, nsn: ''),
+      ),
+      onChanged: onChanged,
+      countrySelectorNavigator: const CountrySelectorNavigator.page(),
+      validator: PhoneValidator.compose([
+        PhoneValidator.required(context, errorText: 'Phone number is required'),
+        PhoneValidator.valid(context),
+      ]),
+      decoration: InputDecoration(
+        hintText: 'Phone number',
+        border: const OutlineInputBorder(
+          borderRadius: PhoneInputRowStyles.borderRadius,
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: PhoneInputRowStyles.borderRadius,
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.primary,
+            width: PhoneInputRowStyles.borderWidth,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: PhoneInputRowStyles.borderRadius,
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.error,
+            width: PhoneInputRowStyles.borderWidth,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: PhoneInputRowStyles.borderRadius,
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.error,
+            width: PhoneInputRowStyles.borderWidth,
+          ),
+        ),
+        filled: true,
+        fillColor: Theme.of(context).colorScheme.tertiaryContainer,
+      ),
+    );
+  }
+}
---
+++ lib/widgets/phone_input_row/phone_input_row_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PhoneInputRowStyles {
+  static const double borderWidth = 1.0;
+
+  static const BorderRadius borderRadius =
+      BorderRadius.all(Radius.circular(15.0));
+}
