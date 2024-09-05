4f5b9d461ad72645453e311b9a30d9e1c70a9255
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-09-05 23:05:38 +0700

    EF-04: Add model folder

---
+++ lib/model/parking/parking.dart
@@ -0,0 +1,61 @@
+import 'package:ecoparking_flutter/model/parking/shift_price.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'parking.g.dart';
+
+@JsonSerializable()
+class Parking with EquatableMixin {
+  final String id;
+  @JsonKey(name: 'parking_name')
+  final String parkingName;
+  final String address;
+  final double latitude;
+  final double longitude;
+  @JsonKey(name: 'total_slot')
+  final int totalSlot;
+  @JsonKey(name: 'available_slot')
+  final int availableSlot;
+  @JsonKey(name: 'price_per_hour')
+  final ShiftPrice? pricePerHour;
+  @JsonKey(name: 'price_per_day')
+  final double? pricePerDay;
+  @JsonKey(name: 'price_per_month')
+  final double? pricePerMonth;
+  @JsonKey(name: 'price_per_year')
+  final double? pricePerYear;
+
+  Parking({
+    required this.id,
+    required this.parkingName,
+    required this.address,
+    required this.latitude,
+    required this.longitude,
+    required this.totalSlot,
+    required this.availableSlot,
+    this.pricePerHour,
+    this.pricePerDay,
+    this.pricePerMonth,
+    this.pricePerYear,
+  });
+
+  factory Parking.fromJson(Map<String, dynamic> json) =>
+      _$ParkingFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ParkingToJson(this);
+
+  @override
+  List<Object?> get props => [
+        id,
+        parkingName,
+        address,
+        latitude,
+        longitude,
+        totalSlot,
+        availableSlot,
+        pricePerHour,
+        pricePerDay,
+        pricePerMonth,
+        pricePerYear,
+      ];
+}
