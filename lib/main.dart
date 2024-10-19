a7db84a38d6dc6edf1515857459ef59952d37ded
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-10-20 00:32:11 +0700

    EF-09: Re-config flutter map

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ecoparking_flutter/config/app_routes.dart';
 import 'package:ecoparking_flutter/config/env_loader.dart';
 import 'package:ecoparking_flutter/config/themes.dart';
 import 'package:ecoparking_flutter/di/global/get_it_initializer.dart';
+import 'package:ecoparking_flutter/utils/platform_infos.dart';
 import 'package:ecoparking_flutter/widgets/theme_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -11,7 +12,11 @@ import 'package:loggy/loggy.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   GetItInitializer().setUp();
-  await dotenv.load(fileName: EnvLoader.envFileName);
+  if (PlatformInfos.isRelease) {
+    await dotenv.load(mergeWith: EnvLoader.compileTimeEnvironment);
+  } else {
+    await dotenv.load(fileName: EnvLoader.envFileName);
+  }
   Loggy.initLoggy(
     logPrinter: const PrettyPrinter(
       showColors: true,
