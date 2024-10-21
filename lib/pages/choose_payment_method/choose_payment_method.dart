ff8c4a5741b2a295c089035a4467e6c7de8edfea
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-10-21 22:51:03 +0700

    EF-08: Add static UI for choose payment method screen

--- lib/pages/choose_payment_method/choose_payment_method.dart
+++ lib/pages/choose_payment_method/choose_payment_method.dart
@@ -1,3 +1,4 @@
+import 'package:ecoparking_flutter/model/payment/e_wallet.dart';
 import 'package:ecoparking_flutter/pages/choose_payment_method/choose_payment_method_view.dart';
 import 'package:ecoparking_flutter/pages/review_summary/review_summary.dart';
 import 'package:ecoparking_flutter/utils/logging/custom_logger.dart';
@@ -13,6 +14,10 @@ class ChoosePaymentMethod extends StatefulWidget {
 
 class ChoosePaymentMethodController extends State<ChoosePaymentMethod>
     with ControllerLoggy {
+  final List<EWallet> paymentMethods = [EWallet.vnpay];
+
+  final selectedPaymentMethod = ValueNotifier<EWallet?>(null);
+
   @override
   void initState() {
     super.initState();
@@ -21,9 +26,15 @@ class ChoosePaymentMethodController extends State<ChoosePaymentMethod>
   @override
   void dispose() {
     super.dispose();
+    selectedPaymentMethod.value = null;
+    selectedPaymentMethod.dispose();
   }
 
-  void onPressedSelectPaymentMethod() {
+  void selectPaymentMethod(EWallet paymentMethod) {
+    selectedPaymentMethod.value = paymentMethod;
+  }
+
+  void onPressedContinue() {
     loggy.info('Select Payment Method tapped');
 
     showDialog(
