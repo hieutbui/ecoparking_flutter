39a000cb4e2d9a9a6efe1869e0e14b80a7e0073d
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-11-03 11:52:24 +0700

    EF-13: Add static UI for booking screen

--- lib/pages/my_tickets/widgets/list_ticket.dart
+++ lib/pages/my_tickets/widgets/list_ticket.dart
@@ -46,23 +46,33 @@ class ListTicket extends StatelessWidget {
         }
 
         if (getUserTicketState is GetUserTicketsSuccess) {
-          return ListView.separated(
-            itemCount: getUserTicketState.tickets.length,
-            itemBuilder: (context, index) {
-              final ticket = getUserTicketState.tickets[index];
-
-              return TicketCard(
-                ticket: ticket,
-                page: page,
-                onCancelBooking: onCancelBooking,
-                onViewTicket: onViewTicket,
-              );
-            },
-            separatorBuilder: (context, index) {
-              return const Divider(
-                height: 8.0,
-              );
-            },
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35.0),
+            child: ListView.separated(
+              shrinkWrap: true,
+              itemCount: getUserTicketState.tickets.length,
+              itemBuilder: (context, index) {
+                final ticket = getUserTicketState.tickets[index];
+            
+                return Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFF9F9F9),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: TicketCard(
+                    ticket: ticket,
+                    page: page,
+                    onCancelBooking: onCancelBooking,
+                    onViewTicket: onViewTicket,
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  height: 8.0,
+                );
+              },
+            ),
           );
         }
 
