5d28100947a2065b493f7c219d64a9b7363dd3ce
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-11-03 16:38:40 +0700

    EF-12: Initialize API call

--- lib/data/repository/user_favorite_parkings/user_favorite_parkings_repository_impl.dart
+++ lib/data/repository/user_favorite_parkings/user_favorite_parkings_repository_impl.dart
@@ -3,7 +3,8 @@ import 'package:ecoparking_flutter/di/global/get_it_initializer.dart';
 import 'package:ecoparking_flutter/domain/repository/user_favorite_parkings/user_favorite_parkings_repository.dart';
 import 'package:ecoparking_flutter/model/account/favorite_parkings.dart';
 
-class UserFavoriteParkingsImpl implements UserFavoriteParkingsRepository {
+class UserFavoriteParkingsRepositoryImpl
+    implements UserFavoriteParkingsRepository {
   final UserFavoriteParkingsDatasource userFavoriteParkingsDatasource =
       getIt.get<UserFavoriteParkingsDatasource>();
 
