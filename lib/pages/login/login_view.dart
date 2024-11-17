d3350241cad98646d1074be97f5a5d6efcc390d0
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2024-11-17 17:02:10 +0700

    EF-14: Remove Forgot Password

--- lib/pages/login/login_view.dart
+++ lib/pages/login/login_view.dart
@@ -98,15 +98,15 @@ class LoginView extends StatelessWidget {
                     const SizedBox(
                       height: LoginViewStyles.signInButtonBottomSpacing,
                     ),
-                    TextButton(
-                      onPressed: controller.onForgotPasswordPressed,
-                      child: Text(
-                        'Forgot the password?',
-                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                      ),
-                    ),
+                    // TextButton(
+                    //   onPressed: controller.onForgotPasswordPressed,
+                    //   child: Text(
+                    //     'Forgot the password?',
+                    //     style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    //           color: Theme.of(context).colorScheme.primary,
+                    //         ),
+                    //   ),
+                    // ),
                     const SizedBox(
                       height: LoginViewStyles.forgotPasswordBottomSpacing,
                     ),
