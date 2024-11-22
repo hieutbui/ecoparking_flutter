462a1f7ba8db263d0e5f30492077998a5dc973ae
Hieu Bui <hieu.bui@base.vn>
2024-11-22 17:29:28 +0700

    Release v0.0.7: Analyzing codebase

--- lib/data/repository/user_vehicles/user_vehicles_repository_impl.dart
+++ lib/data/repository/user_vehicles/user_vehicles_repository_impl.dart
@@ -1,7 +1,6 @@
 import 'package:ecoparking_flutter/data/datasource/user_vehicles/user_vehicles_datasource.dart';
 import 'package:ecoparking_flutter/di/global/get_it_initializer.dart';
 import 'package:ecoparking_flutter/domain/repository/user_vehicles/user_vehicles_repository.dart';
-import 'package:ecoparking_flutter/model/account/vehicle.dart';
 
 class UserVehiclesRepositoryImpl implements UserVehiclesRepository {
   final UserVehiclesDataSource userVehiclesDataSource =
